895d7710a10a0c5d788db742725600a89481a19f
evgenynik <evgenynik92@gmail.com>
2024-07-29 12:34:34 +0300

    removed unused k8s secret

--- tf_files/acm-certificate.tf
+++ tf_files/acm-certificate.tf
@@ -22,27 +22,3 @@ module "cert" {
   validation_record_ttl             = "60"
   allow_validation_record_overwrite = true
 }
-
-# Used to deploy platform-app with https
-resource "kubernetes_secret" "cert_arn" {
-  metadata {
-    name      = "cert-arn-secret"
-    namespace = "argocd"  # Ensure this namespace is where ArgoCD can access
-  }
-
-  data = {
-    CERTIFICATE_ARN = module.cert.arn
-  }
-
-  type = "Opaque"
-  depends_on = [
-    kubernetes_namespace.argocd
-  ]
-}
-
-resource "time_sleep" "wait_for_cert_arn_secret" {
-  depends_on = [
-    kubernetes_secret.cert_arn
-  ]
-  create_duration = "10s"  # Adjust as needed
-}
